623b8873955c0b078b9a1792f32b5dfd124208dc
shabani005 <hasan.shabani2005@gmail.com>
2025-11-26 10:52:44 +0300

    generic arena, and fixed fatal overwriting error

--- arena.h
+++ arena.h
@@ -14,9 +14,21 @@ typedef struct {
 } Arena;
 
 Arena *ar_init(size_t cap);
-void * ar_alloc(Arena *a, size_t size);
-void ar_free(Arena *a);
-void arena_reset(Arena *a);
+void * ar_alloc_generic(Arena *a, size_t size);
+void ar_free_generic(Arena *a);
+void ar_reset_generic(Arena *a);
+
+static Arena *default_arena = NULL;
+
+static void ensure_default_arena(void) {
+  if (!default_arena) {
+    default_arena = ar_init(AR_SIZE);
+  }
+}
+
+#define ar_alloc(size)  ( ensure_default_arena(), ar_alloc_generic(default_arena, size) )
+#define ar_free()       ( ensure_default_arena(), ar_free_generic(default_arena) )
+#define ar_reset()      ( ensure_default_arena(), ar_reset_generic(default_arena) )
 
 #ifdef AR_IMPLEMENTATION
 // TODO: default static ARENA with AR_SIZE 
@@ -30,17 +42,21 @@ Arena *ar_init(size_t cap){
   return a;
 }
 
-void *ar_alloc(Arena *a, size_t size){
+void *ar_alloc_generic(Arena *a, size_t size){
+  if (a->size + size > a->capacity) { 
+    fprintf(stderr, "Writing into out of bounds memory\n");
+    abort();
+  }
   void* ptr = a->base + a->size;
   a->size += size;
   return ptr;
 }
 
-void arena_reset(Arena *a){
+void ar_reset_generic(Arena *a){
   a->size = 0; // We just overwrite old data
 }
 
-void ar_free(Arena *a){
+void ar_free_generic(Arena *a){
   if (a->base){
     munmap(a->base, a->capacity);
     free(a);
--- examples/alloc.c
+++ examples/alloc.c
@@ -1,13 +1,29 @@
+#include <stddef.h>
 #define AR_IMPLEMENTATION
 #include "../arena.h"
 
+#undef AR_SIZE
+#define AR_SIZE 10242239238238328
+
+#define VEC_CAP 1000
+
 int main(void){
-  Arena *ar = ar_init(1024);
-  
-  int *x = ar_alloc(ar, sizeof(int));
+    
+  int *x = ar_alloc(sizeof(int) * VEC_CAP);
   printf("allocated pointer: %p\n", x);
 
-  *x = 5;
-  printf("heap allocated value: %d\n", *x);
+  for (size_t i=0; i<1000; ++i){
+    x[i] = i+1;
+  }
+
+  for (size_t i=0; i<1000; ++i){
+    printf("heap allocated value: %d\n", x[i]);
+  }
+
+  while (1){
+    
+  }
+
+  
   return 0;
 }
