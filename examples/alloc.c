f989310012879e88705d51a2558d1a4298547993
shabani005 <hasan.shabani2005@gmail.com>
2025-11-26 10:55:12 +0300

    generic arena, and fixed fatal overwriting error

--- examples/alloc.c
+++ examples/alloc.c
@@ -2,21 +2,18 @@
 #define AR_IMPLEMENTATION
 #include "../arena.h"
 
-#undef AR_SIZE
-#define AR_SIZE 10242239238238328
-
-#define VEC_CAP 1000
+#define VEC_CAP 10
 
 int main(void){
     
   int *x = ar_alloc(sizeof(int) * VEC_CAP);
   printf("allocated pointer: %p\n", x);
 
-  for (size_t i=0; i<1000; ++i){
+  for (size_t i=0; i<10; ++i){
     x[i] = i+1;
   }
 
-  for (size_t i=0; i<1000; ++i){
+  for (size_t i=0; i<10; ++i){
     printf("heap allocated value: %d\n", x[i]);
   }
 
